f45b1acfa8cb939d3a161bfb492d457f7e98706e
Kongnyu Roger <kongnyuroger711@gmail.com>
2022-09-06 22:28:06 +0100

    fetching api

--- src/App.js
+++ src/App.js
@@ -9,14 +9,17 @@ function App() {
 
   useEffect(()=>{
     getRecipes()
-  },[])
+  }, [])
+  
+
 
   const getRecipes = async() => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=chicken`);
     const data = await response.json()
     console.log(data)
   }
-
+  
+  
   return (
     <div className="App">
       <form className='search-form'>
